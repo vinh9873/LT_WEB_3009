09401e0b5bc2ee6d7e9eba87625b15f12dcb4a44
vinh9873 <nguyenvinh9837@gmail.com>
2024-10-01 17:09:10 +0700

    CRUD Add insert&update&delete

--- src/main/java/vn/ute/controllers/admin/CategoryController.java
+++ src/main/java/vn/ute/controllers/admin/CategoryController.java
@@ -12,7 +12,9 @@ import vn.ute.models.CategoryModel;
 import vn.ute.services.ICategoryService;
 import vn.ute.services.impl.CategoryServiceImpl;
 
-@WebServlet(urlPatterns = {"/admin/categories"})
+@WebServlet(urlPatterns = {"/admin/categories","/admin/category/add",
+		"/admin/category/insert","/admin/category/edit","/admin/category/update",
+		"/admin/category/delete","/admin/category/seach"})
 public class CategoryController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -20,9 +22,80 @@ public class CategoryController extends HttpServlet {
    
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		List<CategoryModel> list = cateService.findALL();
-		req.setAttribute("listcate",list);
-		req.getRequestDispatcher("/views/admin/category-list.jsp").forward(req, resp);
+		
+		String url = req.getRequestURI();
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");;
+		
+		if (url.contains("categories")) {
+		   
+			List<CategoryModel> list = cateService.findALL();
+			req.setAttribute("listcate",list);
+			req.getRequestDispatcher("/views/admin/category-list.jsp").forward(req, resp);
+			
+		} else if(url.contains("add")) {
+			
+			req.getRequestDispatcher("/views/admin/category-add.jsp").forward(req, resp);
+			
+		} else if(url.contains("edit")) {
+			
+			int id = Integer.parseInt(req.getParameter("id"));
+			CategoryModel category = cateService.findById(id);
+			
+			req.setAttribute("cate", category);
+			req.getRequestDispatcher("/views/admin/category-edit.jsp").forward(req, resp);
+			
+		} else if(url.contains("delete")) {
+			
+			String id = req.getParameter("id");
+			cateService.delete(Integer.parseInt(id));
+			resp.sendRedirect(req.getContextPath() + "/admin/categories");
+			
+		}
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String url = req.getRequestURI();
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");;
+		
+		if (url.contains("insert")) {
+		   
+			String categoryname = req.getParameter("categoryname");
+			String status = req.getParameter("status");
+			int statuss = Integer.parseInt(status);
+			String images = "https://cdn11.dienmaycholon.vn/filewebdmclnew/DMCL21/Picture//Apro/Apro_product_33149/oppo-a58_main_172_1020.png.webp";
+			
+			CategoryModel category = new CategoryModel();
+			category.setCategoryname(categoryname);
+			category.setImages(images);
+			category.setStatus(statuss);
+			
+			cateService.insert(category);
+			resp.sendRedirect(req.getContextPath() + "/admin/categories");
+			
+		} else if (url.contains("update")) {
+			
+			int categoryid = Integer.parseInt(req.getParameter("categoryid"));
+			String categoryname = req.getParameter("categoryname");
+			String status = req.getParameter("status");
+			int statuss = Integer.parseInt(status);
+			String images = "https://cdn11.dienmaycholon.vn/filewebdmclnew/DMCL21/Picture//Apro/Apro_product_33149/oppo-a58_main_172_1020.png.webp";
+			
+			CategoryModel category = new CategoryModel();
+			category.setCategoryid(categoryid);
+			category.setCategoryname(categoryname);
+			category.setImages(images);
+			category.setStatus(statuss);
+			
+			cateService.update(category);
+			resp.sendRedirect(req.getContextPath() + "/admin/categories");
+			
+		}
+		
 	}
 	
 }
--- src/main/webapp/views/admin/category-list.jsp
+++ src/main/webapp/views/admin/category-list.jsp
@@ -1,6 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@taglib prefix="c" uri="jakarta.tags.core"%>
+
+
+<a href = "${pageContext.request.contextPath }/admin/category/add">Add Category</a>
 <table border="1" width="100%">
 	<tr>
 		<th>STT</th>
@@ -14,19 +17,22 @@
 		<tr>
 			<td>${STT.index+1}</td>
 			<td>
+			
 			<c:if test="${cate.images.substring(0,5) !='https'}">
 					<c:url value="/image?fname=${cate.images}" var="imgUrl"></c:url>
 				</c:if> 
 				<c:if test="${cate.images.substring(0,5) =='https'}">
 					<c:url value="${cate.images}" var="imgUrl"></c:url>
 				</c:if> 
-				<img height="150" width="200" src="${imgUrl}" /></td>
+				<img height="150" width="200" src="${imgUrl}" />
+				
+			</td>
 			<td>${cate.categoryid}</td>
 			<td>${cate.categoryname}</td>
 			<td><c:if test="${cate.status==1}">
-					<span>Active</span>
+					<span>Hoạt động</span>
 				</c:if> <c:if test="${cate.status!=1}">
-					<span>Block</span>
+					<span>Khóa</span>
 				</c:if></td>
 			<td><a
 				href="<c:url value='/admin/category/edit?id=${cate.categoryid}'/>">Sửa</a>
